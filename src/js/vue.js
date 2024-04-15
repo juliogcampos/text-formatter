1b988651cd587f9fad65dde42a963d6ab5b844fe
Júlio Campos <juliogcampos@gmail.com>
2024-04-14 21:31:23 -0300

    copy text to clipboard

--- src/js/vue.js
+++ src/js/vue.js
@@ -17,6 +17,19 @@ var app = new Vue({
     methods: {
         clearTextFormatting() {
             this.textarea.clearTextFormatting = this.textarea.clearTextFormatting.replace(/\s+/g, ' ').trim();
+        },
+
+        copyTextToClipboard() {
+
+            // get the text field
+            let copyText = document.getElementById("textarea");
+
+            // select the text field
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); // for mobile devices
+
+            // copy the text inside the text field
+            navigator.clipboard.writeText(copyText.value);
         }
     }
 })
